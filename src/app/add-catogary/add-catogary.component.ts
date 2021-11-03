a35d35d47685bd1b325774343c077e977327a49b
Vikash <vk112004@gmail.com>
2021-11-03 12:20:57 +0530

    add sjson server and root guard

---
+++ src/app/add-catogary/add-catogary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-catogary',
+  templateUrl: './add-catogary.component.html',
+  styleUrls: ['./add-catogary.component.css']
+})
+export class AddCatogaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AdminModule } from './admin/admin.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DashbordComponent } from './dashbord/dashbord.component';
+import { AddCatogaryComponent } from './add-catogary/add-catogary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashbordComponent
+    DashbordComponent,
+    AddCatogaryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,AdminModule, NgbModule
+    AppRoutingModule,AdminModule, NgbModule,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
